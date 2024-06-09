b045d6594109c50616417469330a241ba2320f24
BreanGraus <grausbrean2020jm@gmail.com>
2024-06-08 19:05:17 -0500

    Agrege supuestamente "Form_Clientes", solo coloqué lo basico, avisen si no hay cambios o algo asi

---
+++ Turky'sGym_Programa/Clientes.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Turky_sGym_Programa
+{
+    partial class Clientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clientes));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_clientes_aniadir = new System.Windows.Forms.Button();
+            this.btn_clientes_inhabilitar = new System.Windows.Forms.Button();
+            this.btn_clientes_buscar = new System.Windows.Forms.Button();
+            this.gtb_clientes_datos = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nose = new System.Windows.Forms.Label();
+            this.btn_clientes_modificar = new System.Windows.Forms.Button();
+            this.btn_cerrar = new System.Windows.Forms.PictureBox();
+            this.btn_minimizar = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gtb_clientes_datos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_cerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_minimizar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel1.Controls.Add(this.btn_clientes_modificar);
+            this.panel1.Controls.Add(this.gtb_clientes_datos);
+            this.panel1.Controls.Add(this.btn_clientes_buscar);
+            this.panel1.Controls.Add(this.btn_clientes_inhabilitar);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_clientes_aniadir);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 474);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.panel2.Controls.Add(this.btn_minimizar);
+            this.panel2.Controls.Add(this.btn_cerrar);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(187, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(775, 31);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CLIENTES";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(193, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(757, 413);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(44, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(89, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_clientes_aniadir
+            // 
+            this.btn_clientes_aniadir.Location = new System.Drawing.Point(23, 136);
+            this.btn_clientes_aniadir.Name = "btn_clientes_aniadir";
+            this.btn_clientes_aniadir.Size = new System.Drawing.Size(39, 23);
+            this.btn_clientes_aniadir.TabIndex = 4;
+            this.btn_clientes_aniadir.Text = "+";
+            this.btn_clientes_aniadir.UseVisualStyleBackColor = true;
+            // 
+            // btn_clientes_inhabilitar
+            // 
+            this.btn_clientes_inhabilitar.Location = new System.Drawing.Point(117, 136);
+            this.btn_clientes_inhabilitar.Name = "btn_clientes_inhabilitar";
+            this.btn_clientes_inhabilitar.Size = new System.Drawing.Size(39, 23);
+            this.btn_clientes_inhabilitar.TabIndex = 5;
+            this.btn_clientes_inhabilitar.Text = "-";
+            this.btn_clientes_inhabilitar.UseVisualStyleBackColor = true;
+            // 
+            // btn_clientes_buscar
+            // 
+            this.btn_clientes_buscar.Location = new System.Drawing.Point(44, 165);
+            this.btn_clientes_buscar.Name = "btn_clientes_buscar";
+            this.btn_clientes_buscar.Size = new System.Drawing.Size(89, 23);
+            this.btn_clientes_buscar.TabIndex = 6;
+            this.btn_clientes_buscar.Text = "🔎";
+            this.btn_clientes_buscar.UseVisualStyleBackColor = true;
+            // 
+            // gtb_clientes_datos
+            // 
+            this.gtb_clientes_datos.Controls.Add(this.nose);
+            this.gtb_clientes_datos.Controls.Add(this.label3);
+            this.gtb_clientes_datos.Controls.Add(this.label2);
+            this.gtb_clientes_datos.Controls.Add(this.textBox3);
+            this.gtb_clientes_datos.Controls.Add(this.textBox2);
+            this.gtb_clientes_datos.Controls.Add(this.textBox1);
+            this.gtb_clientes_datos.Location = new System.Drawing.Point(12, 214);
+            this.gtb_clientes_datos.Name = "gtb_clientes_datos";
+            this.gtb_clientes_datos.Size = new System.Drawing.Size(159, 236);
+            this.gtb_clientes_datos.TabIndex = 7;
+            this.gtb_clientes_datos.TabStop = false;
+            this.gtb_clientes_datos.Text = "Datos:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(32, 179);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombres";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DNI";
+            // 
+            // nose
+            // 
+            this.nose.AutoSize = true;
+            this.nose.Location = new System.Drawing.Point(8, 160);
+            this.nose.Name = "nose";
+            this.nose.Size = new System.Drawing.Size(106, 16);
+            this.nose.TabIndex = 5;
+            this.nose.Text = "Tipo Membresia";
+            // 
+            // btn_clientes_modificar
+            // 
+            this.btn_clientes_modificar.Location = new System.Drawing.Point(72, 136);
+            this.btn_clientes_modificar.Name = "btn_clientes_modificar";
+            this.btn_clientes_modificar.Size = new System.Drawing.Size(39, 23);
+            this.btn_clientes_modificar.TabIndex = 8;
+            this.btn_clientes_modificar.Text = "🔧";
+            this.btn_clientes_modificar.UseVisualStyleBackColor = true;
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.Image = ((System.Drawing.Image)(resources.GetObject("btn_cerrar.Image")));
+            this.btn_cerrar.Location = new System.Drawing.Point(737, 1);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(26, 27);
+            this.btn_cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn_cerrar.TabIndex = 0;
+            this.btn_cerrar.TabStop = false;
+            // 
+            // btn_minimizar
+            // 
+            this.btn_minimizar.Image = ((System.Drawing.Image)(resources.GetObject("btn_minimizar.Image")));
+            this.btn_minimizar.Location = new System.Drawing.Point(705, 0);
+            this.btn_minimizar.Name = "btn_minimizar";
+            this.btn_minimizar.Size = new System.Drawing.Size(26, 27);
+            this.btn_minimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btn_minimizar.TabIndex = 1;
+            this.btn_minimizar.TabStop = false;
+            // 
+            // Clientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 474);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Clientes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clientes";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gtb_clientes_datos.ResumeLayout(false);
+            this.gtb_clientes_datos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_cerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_minimizar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_clientes_aniadir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_clientes_inhabilitar;
+        private System.Windows.Forms.Button btn_clientes_buscar;
+        private System.Windows.Forms.GroupBox gtb_clientes_datos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label nose;
+        private System.Windows.Forms.Button btn_clientes_modificar;
+        private System.Windows.Forms.PictureBox btn_minimizar;
+        private System.Windows.Forms.PictureBox btn_cerrar;
+    }
+}
